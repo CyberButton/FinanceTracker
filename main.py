fa9a01e6b86792637b5c086f48bc931562fa8152
CyberButton <kelesovnurtore2@gmail.com>
2022-08-30 17:11:19 +0600

    added option to delete the table

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ connection = sqlite3.connect("expenses.db")
 crsr = connection.cursor()
 
 # execute the statement
-crsr.execute("CREATE TABLE IF NOT EXISTS expenses (Goods_or_Services VARCHAR(35), prices VARCHAR(10), dates VARCHAR("
+crsr.execute("CREATE TABLE IF NOT EXISTS expenses (expense_id INTEGER PRIMARY KEY, Goods_or_Services VARCHAR(35), prices VARCHAR(10), dates VARCHAR("
              "15), expense_types VARCHAR(30));")
 
 while option != 0:
@@ -27,6 +27,7 @@ while option != 0:
     print("2. Add Entertainment Expense")
     print("3. Add Other Expense")
     print("4. Show and Save the Expense Report")
+    print("5. Delete everything and Empty the table")
     print("0. Exit")
     option = int(input("Choose an option:\n"))
 
@@ -48,9 +49,13 @@ while option != 0:
         expense_type = "OTHER"
     elif option == 4:
         get_info()
+       # print()
+    elif option == 5:
+        print("Emptying the table")
+        crsr.execute("DELETE FROM expenses;")
 
     else:
-        print("PLEASE SELECT ONLY OPTIONS 0, 1, 2, 3, 4")
+        print("PLEASE SELECT ONLY OPTIONS 0, 1, 2, 3, 4, 5")
 
     if option == 1 or option == 2 or option == 3:
         goods_or_services = input("Give specific name of your spending in category " + expense_type + ":\n")
@@ -59,4 +64,4 @@ while option != 0:
 
         crsr.execute("INSERT INTO expenses (Goods_or_Services, prices, dates, expense_types) VALUES(?, ?, ?, ?)", (goods_or_services, price, today, expense_type))
         connection.commit()
-        print()
+
